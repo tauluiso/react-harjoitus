2506a9570942bb321a3ca123253b6005b4d15172
SomeKnight20 <teemu.huttunen20@gmail.com>
2023-04-19 18:16:20 +0300

    boardin muokkaus

--- react-harjoitus/src/ristinolla/Board.jsx
+++ react-harjoitus/src/ristinolla/Board.jsx
@@ -2,10 +2,42 @@ import React from "react";
 
 import {Square} from "./Square";
 
-export const Board =()=>{
+export const Board =({gameState})=>{
     return(
-        <div>
-            
-        </div>
+            <div>  
+                <div className="row">
+                    <Square className="b-bottom-r"
+                    state = {gameState[0]}
+                    />
+                    <Square className="b-bottom-r"
+                    state = {gameState[1]}
+                    />
+                    <Square className="b-bottom"
+                    state = {gameState[2]}
+                    />
+                </div>
+                <div className="row">  
+                    <Square className="b-bottom-r"
+                    state = {gameState[3]}
+                    />
+                    <Square className="b-bottom-r"
+                    state = {gameState[4]}
+                    />
+                    <Square className="b-bottom"
+                    state = {gameState[5]}
+                    />
+                </div>
+                <div className="row">  
+                    <Square className="b-right"
+                    state = {gameState[6]}
+                    />
+                    <Square className="b-right"
+                    state = {gameState[7]}
+                    />
+                    <Square
+                    state = {gameState[8]}
+                    />
+                </div>
+            </div>
     );
 }
--- react-harjoitus/src/ristinolla/Ristinolla.jsx
+++ react-harjoitus/src/ristinolla/Ristinolla.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import './Ristinolla.css'
 import {Board} from "./Board";
+import { useState } from "react";
+
+const initialBoard=['', '', '', '', '', '', '', '', ''];
 
 export const Ristinolla =()=>{
+    const[gameState, setGameState] = useState(initialBoard);
+    const[isXTurn, setIsXTurn] = useState(true);
+ 
+    const onSquareClick =(index)=>{
+        let strings = Array.from(gameState);
+        if(strings[index] !==''){
+            return;
+        }
+    }
     return(
         <div>
             <h1>Ristinolla</h1>
-            <Board/>
+            <Board gameState={gameState}/>
         </div>
     );
 }
